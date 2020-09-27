4925aa49073d0809cc608cacfe3de721cae9d27a
itRunTester <itrun.tester@gmail.com>
2020-09-27 20:04:52 +0200

    Initial commit

---
+++ src/test/java/pl/zadora/thymeleafhello/ThymeleafHelloControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zadora.thymeleafhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafHelloControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
