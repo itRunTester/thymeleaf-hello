3562f57ed5fbcdb44f141caffa2b54f8cff833b6
itRunTester <itrun.tester@gmail.com>
2020-09-28 21:02:23 +0200

    model-new branch

--- src/main/java/pl/zadora/thymeleafhello/HelloController.java
+++ src/main/java/pl/zadora/thymeleafhello/HelloController.java
@@ -1,13 +1,15 @@
 package pl.zadora.thymeleafhello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public String get() {
+    public String get()(Model model) {
+        model.addAttribute("name", "Piorun");
         return "hello";
     }
 }
